23d8780aa967e4c798b3021db639016e7ecb914f
elh <elh.git@proton.me>
2026-08-16 03:19:06 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Random Status Catto's
+
+A very useless website which generates random HTTP status cats using [http.cat](https://http.cat/)
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import statuses from "statuses";
+
+function renderCatto(statusCode, statusMessage) {
+    const cattoActivity = document.querySelector(".catto-activity")
+    const catto = document.querySelector(".clipped-catto")
+
+    cattoActivity.textContent = `${statusCode}, ${statusMessage}.`
+    catto.src = `https://http.cat/${statusCode}`
+    catto.alt = `A cat illustrating HTTP ${statusCode} ${statusMessage}`
+
+    catto.addEventListener("load", () => {
+        catto.classList.toggle(
+            "clipped-tall-catto",
+            catto.naturalHeight > catto.naturalWidth
+        );
+    });
+
+    document.querySelector(".catto-box").classList.remove("no-js")
+}
+
+const code = statuses.codes[Math.floor(Math.random() * statuses.codes.length)];
+setTimeout(() => renderCatto(code, statuses(code)), 500)
